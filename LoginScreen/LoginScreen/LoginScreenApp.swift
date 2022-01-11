1b8e1c2046d459ecb73495fa2e79d578fc3847bd
SharonB9 <92518636+SharonB9@users.noreply.github.com>
2022-01-12 00:58:56 +0100

    initial commit

---
+++ LoginScreen/LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Decagon on 12/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
