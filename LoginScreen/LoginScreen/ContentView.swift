ffd99638a4f284198e16a2c1374ffa04ba53269c
SharonB9 <92518636+SharonB9@users.noreply.github.com>
2022-01-13 00:37:25 +0100

    Login screen UI has been created

--- LoginScreen/LoginScreen/ContentView.swift
+++ LoginScreen/LoginScreen/ContentView.swift
@@ -6,19 +6,23 @@
 //rgba 0.9, 0.9, 0.9, 1.0
 
 import SwiftUI
+let buttonColor = Color(red: 115/255, green: 61/255, blue: 71/255)
 let color = Color(red: 191/255, green: 155/255, blue: 155/255)
 let textFieldColor = Color(red: 0.9, green: 0.9, blue: 0.9)
 struct ContentView: View {
     @State var username: String = ""
-    @State var gmail: String = ""
     @State var password: String = ""
+    @State var email: String = ""
+   
     var body: some View {
         VStack{
             logoImage()
+            VStack{
             Text("Username")
+                    .padding(-112)
+                
             .foregroundColor(.gray)
             .background(.white)
-            .padding()
             HStack{
                 HStack {
                 Label(
@@ -33,9 +37,55 @@ struct ContentView: View {
             .padding()
             .overlay(RoundedRectangle(cornerRadius: 10).stroke(color, lineWidth: 1))
             }
+            }
             .padding(.leading, 36)
             .padding(.trailing, 37)
-        }
+            .padding(.bottom, 24)
+            HStack{
+                HStack {
+                    Image("keyIcon")
+                        .resizable()
+                        .clipped()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 17, height: 17)
+              TextField("", text: $password)
+                    Image(systemName: "eye.fill")
+                        .foregroundColor(color)
+           }
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(textFieldColor, lineWidth: 0.5))
+            }
+            .padding(.leading, 36)
+            .padding(.trailing, 37)
+            .padding(.bottom, 24)
+            HStack{
+                HStack {
+                    Image(systemName: "at")
+                        .foregroundColor(.gray)
+              TextField("", text: $email)
+           }
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(textFieldColor, lineWidth: 0.5))
+            }
+            .padding(.leading, 36)
+            .padding(.trailing, 37)
+            .padding(.bottom, 33)
+            Text("Forgotten password?")
+                .foregroundColor(color)
+                .font(.system(size: 15))
+                .fontWeight(.semibold)
+                .padding(.leading, 189)
+                .padding(.trailing, 34)
+            Button {
+                print("Sign up button was tapped")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(buttonColor)
+                    
+            }
+    }
     }
 }
 struct ContentView_Previews: PreviewProvider {
