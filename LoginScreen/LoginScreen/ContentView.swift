54be53cb5c500ff3ef1aa6764ff58bf8e9839c63
SharonB9 <92518636+SharonB9@users.noreply.github.com>
2022-01-12 22:28:56 +0100

    userNameTextField created

--- LoginScreen/LoginScreen/ContentView.swift
+++ LoginScreen/LoginScreen/ContentView.swift
@@ -3,18 +3,41 @@
 //  LoginScreen
 //
 //  Created by Decagon on 12/01/2022.
-//
+//rgba 0.9, 0.9, 0.9, 1.0
 
 import SwiftUI
-let color = UIColor(red: 191/255, green: 155/255, blue: 155/255, alpha: 1/255)
+let color = Color(red: 191/255, green: 155/255, blue: 155/255)
+let textFieldColor = Color(red: 0.9, green: 0.9, blue: 0.9)
 struct ContentView: View {
+    @State var username: String = ""
+    @State var gmail: String = ""
+    @State var password: String = ""
     var body: some View {
         VStack{
             logoImage()
+            Text("Username")
+            .foregroundColor(.gray)
+            .background(.white)
+            .padding()
+            HStack{
+                HStack {
+                Label(
+                    title: {Text("TheEdvorian")
+                    .background(textFieldColor)
+                    .font(.system(size: 20))},
+                    icon: {Image(systemName: "person")
+                    .foregroundColor(.gray)
+                })
+              TextField("", text: $username)
+           }
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(color, lineWidth: 1))
+            }
+            .padding(.leading, 36)
+            .padding(.trailing, 37)
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -26,9 +49,12 @@ struct logoImage: View {
         .resizable()
         .frame(width: 156, height: 39.22)
         .clipped()
+        .aspectRatio(contentMode: .fill)
         .padding(.bottom, 107.78)
         .padding(.top, 74)
         .padding(.leading, 110)
         .padding(.trailing, 109)
 }
 }
+
+
