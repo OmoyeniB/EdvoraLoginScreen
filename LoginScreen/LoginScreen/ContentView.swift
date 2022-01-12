745bb6df8c3a9cad7d50c0cbfa371d60099c94ca
SharonB9 <92518636+SharonB9@users.noreply.github.com>
2022-01-12 02:29:18 +0100

    logoImage added

--- LoginScreen/LoginScreen/ContentView.swift
+++ LoginScreen/LoginScreen/ContentView.swift
@@ -10,7 +10,7 @@ let color = UIColor(red: 191/255, green: 155/255, blue: 155/255, alpha: 1/255)
 struct ContentView: View {
     var body: some View {
         VStack{
-            Image(uiImage: UIImage(named: "logo"))
+            logoImage()
         }
     }
 }
@@ -19,4 +19,16 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct logoImage: View {
+    var body: some View{
+    Image("logo")
+        .resizable()
+        .frame(width: 156, height: 39.22)
+        .clipped()
+        .padding(.bottom, 107.78)
+        .padding(.top, 74)
+        .padding(.leading, 110)
+        .padding(.trailing, 109)
+}
 }
