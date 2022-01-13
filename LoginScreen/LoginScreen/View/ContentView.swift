0e4c854004225906b034cd81d5befbfebe356437
SharonB9 <92518636+SharonB9@users.noreply.github.com>
2022-01-14 00:13:36 +0100

    UI design has been completed

--- LoginScreen/LoginScreen/View/ContentView.swift
+++ LoginScreen/LoginScreen/View/ContentView.swift
@@ -1,25 +1,16 @@
-//
-//  ContentView.swift
-//  LoginScreen
-//
-//  Created by Decagon on 12/01/2022.
-//rgba 0.9, 0.9, 0.9, 1.0
-
 import SwiftUI
+
 let buttonColor = Color(red: 115/255, green: 61/255, blue: 71/255)
 let color = Color(red: 191/255, green: 155/255, blue: 155/255)
 let textFieldColor = Color(red: 180/255, green: 180/255, blue: 180/255)
 
-
 struct ContentView: View {
-     var body: some View {
+    var body: some View {
         VStack(){
-//            Spacer()
             logoImage()
             ZStack (alignment: .topLeading){
                 HStack{
                     HStack {
-                        edvorianlabel()
                         usernameTextField()
                     }
                     .padding()
@@ -30,56 +21,40 @@ struct ContentView: View {
             }
             .padding(.bottom, 24)
             ZStack(alignment: .topLeading){
-            HStack{
-                HStack {
-                   image()
-                }
+                HStack{
+                    HStack {
+                        image()
+                    }
                     .padding()
                     .overlay(RoundedRectangle(cornerRadius: 10)
-                    .stroke(textFieldColor, lineWidth: 1))
+                                .stroke(textFieldColor, lineWidth: 1))
                     .frame(width: 302, height: 52)
-            }
+                }
                 passwordText()
             }
             .padding(.bottom, 24)
             ZStack (alignment: .topLeading){
-            HStack{
-                HStack {
-                    emailImage()
+                HStack{
+                    HStack {
+                        emailImage()
+                    }
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(textFieldColor, lineWidth: 1))
+                    .frame(width: 302, height: 52, alignment: .leading)
                 }
-                .padding()
-                .overlay(RoundedRectangle(cornerRadius: 10).stroke(textFieldColor, lineWidth: 1))
-                .frame(width: 302, height: 52, alignment: .leading)
-            }
-               emailAddressText()
+                emailAddressText()
             }
             .padding(.bottom, 25)
-            
-            Text("Forgotten password?")
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
-                .foregroundColor(color)
-                .padding(.leading, 170)
-                .padding(.trailing, 34)
-                .padding(.bottom, 59)
-            Text("LOGIN")
-                .font(.headline)
-                .foregroundColor(.white)
-                .frame(width: 302, height: 52, alignment: .center)
-                .background(buttonColor)
-                .cornerRadius(12.99)
-            .padding(.bottom, 30)
-            HStack{
-                Text("Don't have an account?")
-                    .foregroundColor(.gray)
-                    .padding(.bottom, 31)
-                Text("SignUp")
-                    .foregroundColor(color)
-                    .font(.system(size: 15).weight(.semibold))
-                    .padding(.bottom, 31)
+            forgottenPasswordText()
+            Button(action: {
+                print("Login button has been clicked")
+            }){
+                loginButton()
             }
-            Spacer(minLength: 31)
-            
+            HStack{
+                footText()
+            }
+            .padding(.bottom,31)
         }
     }
 }
--- LoginScreen/LoginScreen/View/LoginScreenItem.swift
+++ LoginScreen/LoginScreen/View/LoginScreenItem.swift
@@ -2,31 +2,24 @@ import SwiftUI
 
 struct logoImage: View {
     var body: some View{
-    Image("logos")
-        .resizable()
-        .frame(width: 156, height: 39.22)
-        .clipped()
-        .aspectRatio(contentMode: .fill)
-        .padding(.bottom, 107.78)
-        .padding(.top, 75)
-        .padding(.leading, 110)
-        .padding(.trailing, 109)
-    }
-}
-struct edvorianlabel: View {
-    var body: some View{
-        Label(
-            title: {Text("TheEdvorian")
-                .foregroundColor(textFieldColor)
-                .font(.system(size: 17))},
-            icon: {Image(systemName: "person.fill")
-                .foregroundColor(textFieldColor)})
+        Image("logos")
+            .resizable()
+            .frame(width: 156, height: 39.22)
+            .clipped()
+            .aspectRatio(contentMode: .fill)
+            .padding(.bottom, 107.78)
+            .padding(.top, 75)
+            .padding(.leading, 110)
+            .padding(.trailing, 109)
     }
 }
 struct usernameTextField: View {
     @State var username: String = ""
     var body: some View{
+        Image(systemName: "person.fill")
+            .foregroundColor(textFieldColor)
         TextField("", text: $username)
+            .font(.system(size: 17))
             .autocapitalization(.none)
             .disableAutocorrection(true)
     }
@@ -35,7 +28,7 @@ struct userNameText: View {
     var body: some View{
         Rectangle()
             .fill(.white)
-            .frame(width: 90, height: 15)
+            .frame(width: 85, height: 15)
             .offset(x: 16)
             .padding(.top, -10)
         Text("Username")
@@ -43,7 +36,7 @@ struct userNameText: View {
             .offset(x: 19)
             .padding(.top, -10)
             .padding(.horizontal, 4)
-    
+            .font(.system(size: 15))
     }
 }
 struct image: View {
@@ -66,12 +59,15 @@ struct passwordText: View {
     var body: some View{
         Rectangle()
             .fill(.white)
-            .offset()
+            .offset(x: 16)
+            .padding(.top, -10)
+            .frame(width: 80, height: 15)
         Text("Password")
+            .font(.system(size: 15))
             .foregroundColor(textFieldColor)
-            .background(.white)
             .offset(x: 19)
             .padding(.top, -10)
+            .padding(.horizontal, 4)
     }
 }
 struct emailImage: View {
@@ -86,45 +82,49 @@ struct emailImage: View {
 }
 struct emailAddressText: View {
     var body: some View{
+        Rectangle()
+            .fill(.white)
+            .offset(x: 16)
+            .padding(.top, -8)
+            .frame(width: 110, height: 15)
         Text("Email address")
+            .font(.system(size: 15))
             .foregroundColor(textFieldColor)
-            .background(.white)
             .padding(.top, -10)
             .offset(x: 19)
+            .padding(.horizontal, 4)
     }
 }
-//struct forgottenPasswordText: View {
-//    var body: some View{
-//        Text("Forgotten password?")
-//            .autocapitalization(.none)
-//            .disableAutocorrection(true)
-//            .foregroundColor(color)
-//            .padding(.leading, 189)
-//            .padding(.trailing, 34)
-//            .padding(.bottom, 59)
-//    }
-//}
-//struct loginButton: View {
-//    var body: some View{
-//        Text("LOGIN")
-//            .font(.headline)
-//            .foregroundColor(.white)
-//            .frame(width: 302, height: 52, alignment: .center)
-//            .background(buttonColor)
-//            .cornerRadius(12.99)
-//        .padding(.bottom, 30)
-//    }
-//}
-//struct footText: View {
-//    var body: some View{
-//        Text("Don't have an account?")
-//            .foregroundColor(.gray)
-//        Text("SignUp")
-//            .foregroundColor(color)
-//            .font(.system(size: 15).weight(.semibold))
-//        
-//    }
-//}
-//
-//
-//
+struct forgottenPasswordText: View {
+    var body: some View{
+        Text("Forgotten password?")
+            .autocapitalization(.none)
+            .disableAutocorrection(true)
+            .foregroundColor(color)
+            .padding(.leading, 189)
+            .padding(.trailing, 34)
+            .padding(.bottom, 59)
+    }
+}
+struct loginButton: View {
+    var body: some View{
+        Text("LOGIN")
+            .font(.headline)
+            .foregroundColor(.white)
+            .frame(width: 302, height: 52, alignment: .center)
+            .background(buttonColor)
+            .cornerRadius(12.99)
+            .padding(.bottom, 30)
+    }
+}
+struct footText: View {
+    var body: some View{
+        Text("Don't have an account?")
+            .foregroundColor(.gray)
+            .padding(.bottom, 31)
+        Text("SignUp")
+            .foregroundColor(color)
+            .font(.system(size: 15).weight(.semibold))
+            .padding(.bottom, 31)
+    }
+}
